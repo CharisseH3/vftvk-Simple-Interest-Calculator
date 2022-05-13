6d949deb32dcd7d1eeffbd352e98a2d9cc04e646
CharisseH3 <charissehaas@gmail.com>
2022-05-13 02:10:09 +0000

    changes made from theia lab environment

--- script.js
+++ script.js
@@ -1,14 +1,29 @@
 function compute()
 {
+    //Create variables 
     p = document.getElementById("principal").value;
     var principal = document.getElementById("principal").value;
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
+    
+    //do calculations
     var interest = principal * years * rate /100;
     var year = new Date().getFullYear()+parseInt(years);
     
+    //compound interest formula
+    let compounded = principal * ((1 + ((rate/100)/12)) ** (12*years))
+        
+    //validate the principal-if negative, 0, or blank, alert user 
+    if(principal <= 0){
+        alert("Enter a positive number");
+        document.getElementById("principal").focus();
+        return false;
+    
+    
+}
 
-}        
+
+//display rate by slider     
 function updateRate() 
 {
     var rateval = document.getElementById("rate").value;
@@ -18,5 +33,5 @@ function updateRate()
 
 function onclick() 
 {
-    document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>"
+    document.getElementById("result").innerHTML=`If you deposit ${principal},<br>at an interest rate of ${rate}%<br>You will receive an amount of ${amount},<br>in the year ${year}<br>`
 }
